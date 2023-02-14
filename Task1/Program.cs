d4e0d60d7845364300abc2ad35308d8d2fa8ed9f
Sergey Kucherin <skucherin@yandex.ru>
2023-02-15 01:26:07 +0700

    Decided Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+WriteLine("Является или нет число a квадратом чиcла b");
+Write("Введите число a:  ");
+int a = int.Parse(ReadLine());
+Write("Введите число b:  ");
+int b = int.Parse(ReadLine());
+if (a == b*b)
+{
+    WriteLine("Да");
+}
+else
+{
+    WriteLine("Нет");
+}
