853fb4bf03a1aae9757e54feb4549c2161e4f014
Sergey Kucherin <skucherin@yandex.ru>
2023-02-15 22:05:39 +0700

    Decided Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+using static System.Console;
+Clear();
+Write("Введите число: ");
+int a = int.Parse(ReadLine()!);
+int n = -a;
+
+while (n <= a)
+{
+    Write($" {n} ");
+    n = n + 1;
+}
