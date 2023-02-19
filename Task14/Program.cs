9bb5a80f4b3857a84cf914582a58c9fee7d8899b
Sergey Kucherin <skucherin@yandex.ru>
2023-02-20 00:57:36 +0700

    Decided Task14 correct version2

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -5,11 +5,11 @@ Clear();
 
 int number1 = new Random() .Next(1, 101);
 int number2 = new Random() .Next(1, 101);
-//int number1 = 625;
-//int number2 = 25;
+//int number1 = 9;
+//int number2 = 84;
 WriteLine(number1);
 WriteLine(number2);
-//int result = number1 / number2;
 WriteLine(number1 / number2 == number2 ? number1 % number2 == 0 : "False" );
 WriteLine(number2 / number1 == number1 ? number2 % number1 == 0 : "False" );
-
+//WriteLine(number1 / number2 == number2 ? "number2 квадрат number1"
+        //: number2 / number1 == number1 ? "number1 квадрат number2" : "не являются квадратами друг друга" );
