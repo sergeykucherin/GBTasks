cd2089047cc0cade972f2e27d0f39561cb8f3cc8
Sergey Kucherin <skucherin@yandex.ru>
2023-02-19 23:58:54 +0700

    Decided Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+using static System.Console;
+Clear();
+
+//int number1 = new Random() .Next(1, 101);
+//int number2 = new Random() .Next(1, 101);
+int number1 = 9;
+int number2 = 81;
+WriteLine(number1);
+WriteLine(number2);
+//int result = number1 / number2;
+WriteLine(number1 / number2 == number2 ? number1 % number2 == 0 : "False" );
+WriteLine(number2 / number1 == number1 ? number2 % number1 == 0 : "False" );
+
