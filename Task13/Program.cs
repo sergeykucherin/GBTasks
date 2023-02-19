5ea033d1b5f0c3a73f319e95fd98abe1c7e4b05e
Sergey Kucherin <skucherin@yandex.ru>
2023-02-19 23:22:04 +0700

    Decided Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+using static System.Console;
+Clear();
+
+int number = new Random() .Next(161, 645);
+//int number = 644;
+WriteLine(number);
+WriteLine(number % 7 == 0 ? number % 23 == 0 : "False" );
