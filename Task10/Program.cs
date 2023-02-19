c638c415fccb88d682ad2c4d53e9bb7f71fc570c
Sergey Kucherin <skucherin@yandex.ru>
2023-02-19 21:25:52 +0700

    Decided Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+using static System.Console;
+Clear();
+
+int number = new Random() .Next(100, 1000);
+WriteLine(number);
+Write(number / 100);
+Write(number % 10);
+
+
+
+
