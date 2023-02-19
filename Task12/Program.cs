cbe27f76c84d68b3b89d842c24f53970a30f4c06
Sergey Kucherin <skucherin@yandex.ru>
2023-02-19 22:50:06 +0700

    Decided Task12

---
+++ Task12/Program.cs
@@ -0,0 +1,11 @@
+﻿// Введите 2 числа, если a>b, то показать знак >; если a<b, то показать знак <, если а=б, то показать знак =
+
+using static System.Console;
+Clear();
+
+int number1 = new Random() .Next(1, 11);
+int number2 = new Random() .Next(1, 11);
+WriteLine(number1);
+WriteLine(number2);
+//int result = number1 % number2;
+WriteLine(number1 > number2 ? ">" : number1 < number2 ? "<" : "=" );
